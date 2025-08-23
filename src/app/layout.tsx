ec51cba007438c4953ce57137dab5a778358704a
Josival Salvador <josivaljunior087@gmail.com>
2025-08-23 05:44:51 -0400

    Moldando painel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { ToastContainer } from "react-toastify";
 import Navbar from "./components/Navbar/Navbar";
 import { ReactQueryClientProvider } from './components/ReactQueryClient/ReactQueryClient';
 import { ProdutosProvider } from './components/produtosProvider/produtosProvider';
+import { cookies } from "next/headers";
 
 export const metadata: Metadata = {
   title: "Crochê da T",
@@ -20,12 +21,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = cookies();
+  const hasSession = cookieStore.has("connect.sid");
   return (
     <html lang="pt-br">
       <body className="bg-dark">
         <ReactQueryClientProvider>
             <ProdutosProvider>
-                <Navbar />
+                <Navbar isAuthenticated={hasSession} />
                 {children}
                 <BootstrapClient />
                 <ToastContainer />
