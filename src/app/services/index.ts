0609b531b0a0a71e8782e894325e385a3e09c6df
Josival Salvador <josivaljunior087@gmail.com>
2025-09-16 14:13:46 -0400

    mudando hospedagem

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const produtosApi = axios.create({
-    baseURL: "https://crochedat.up.railway.app/v1",
+    baseURL: "https://crochedat-back.osc-fr1.scalingo.io/v1",
     withCredentials: true,
     /*baseURL: "https://crochedat-back.onrender.com/v1",*/
 });
